a47af5522a224ce8c8e70601f125a80646cb844f
jakeleviathan <81382567+jakeleviathan@users.noreply.github.com>
2026-09-10 02:28:48 -0400

    Wire Blackwall human opening into live server

--- traditional-mud/mud/server.py
+++ traditional-mud/mud/server.py
@@ -6,6 +6,7 @@ from mud.dwarf_start import install_dwarf_content, install_dwarf_runtime
 from mud.forest_elf_nurture import install_forest_elf_nurture_content, install_forest_elf_nurture_runtime
 from mud.forest_elf_stewardship import install_forest_elf_stewardship_content, install_forest_elf_stewardship_runtime
 from mud.goblin_start import goblin_room_augmentations, install_goblin_world
+from mud.human_blackwall_opening import install_human_blackwall_content, install_human_blackwall_runtime
 from mud.troll_start import install_troll_content, install_troll_runtime
 from mud.troll_raid_opening import install_troll_raid_content, install_troll_raid_runtime
 from mud.troll_survivor_choice import (
@@ -23,6 +24,7 @@ install_dwarf_content()
 install_forest_elf_nurture_content()
 install_forest_elf_stewardship_content()
 install_goblin_world()
+install_human_blackwall_content()
 install_troll_content()
 install_troll_raid_content()
 install_troll_survivor_choice_content()
@@ -71,6 +73,7 @@ install_seasonal_runtime(PlayerSession, WORLD)
 install_forest_elf_nurture_runtime(PlayerSession, WORLD)
 install_forest_elf_stewardship_runtime(PlayerSession, WORLD)
 install_dwarf_runtime(PlayerSession, WORLD)
+install_human_blackwall_runtime(PlayerSession, WORLD)
 install_troll_runtime(PlayerSession, WORLD)
 install_troll_raid_runtime(PlayerSession, WORLD)
 install_troll_survivor_choice_runtime(PlayerSession, WORLD)
