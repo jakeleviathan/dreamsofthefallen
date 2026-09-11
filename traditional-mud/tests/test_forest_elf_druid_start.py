a4436148e41dc190a9cbe0141ab09e8387cbece4
jakeleviathan <81382567+jakeleviathan@users.noreply.github.com>
2026-09-11 13:40:38 -0400

    Isolate Forest Elf Druid test ability registry

--- traditional-mud/tests/test_forest_elf_druid_start.py
+++ traditional-mud/tests/test_forest_elf_druid_start.py
@@ -4,6 +4,7 @@ import asyncio
 import unittest
 from dataclasses import dataclass
 
+import mud.mechanics as mechanics
 import mud.quests as quests
 import mud.world as world
 from mud.forest_elf_druid_start import (
@@ -114,6 +115,7 @@ class ForestElfDruidStartTests(unittest.TestCase):
         self.npcs_by_key = dict(world.NPCS_BY_KEY)
         self.quest_tuple = quests.QUESTS
         self.quest_map = dict(quests.QUESTS_BY_KEY)
+        self.druid_abilities = mechanics.FIXED_CLASS_ABILITIES.get("druid", ())
         install_forest_elf_stewardship_content()
         install_forest_elf_home_content()
         install_forest_elf_druid_content()
@@ -128,6 +130,7 @@ class ForestElfDruidStartTests(unittest.TestCase):
         quests.QUESTS = self.quest_tuple
         quests.QUESTS_BY_KEY.clear()
         quests.QUESTS_BY_KEY.update(self.quest_map)
+        mechanics.FIXED_CLASS_ABILITIES["druid"] = self.druid_abilities
 
     def test_content_adds_safe_cozy_warmhouse_and_druid_mentor(self):
         self.assertIs(quests.QUESTS_BY_KEY[FOREST_ELF_DRUID_QUEST_KEY], FOREST_ELF_DRUID_QUEST)
